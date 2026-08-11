aff2c167d8c59a3af6642ef5e393cca68d470aca
ozz-02 <oscarzumaranp027@gmail.com>
2026-08-11 16:07:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ let diag2 = false;
 let msg = false;
 let temp;
 
-const esCelular= ('ontouchstart' in window) || (navigator.maxTouchPoints > 0);
+const esCelular= ('ontouchstart' in window) || (navigator.maxTouchPoints > 0);;
 
 if (esCelular) {
     textoInicio = "presiona para iniciar;)";
